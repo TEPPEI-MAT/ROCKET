24e491dabd5bc6099b59fe1d52af3092842ecd73
TEPPEI-MAT <131732376+TEPPEI-MAT@users.noreply.github.com>
2024-06-26 15:51:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ROCKET',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     description="Rational Oligonucleotide design Calculated with Kinetic parameter for Enhanced in vitro Transcription",
     author="Teppei_Matsuda",
